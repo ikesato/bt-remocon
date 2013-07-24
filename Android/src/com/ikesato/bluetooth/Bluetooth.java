807dab957523720e833e196783e5438348f94f17
ikesato <ikesato76@gmail.com>
2013-07-25 04:33:43 +0900

    implmented scan devices

--- Android/src/com/ikesato/bluetooth/Bluetooth.java
+++ Android/src/com/ikesato/bluetooth/Bluetooth.java
@@ -1,22 +1,88 @@
 package com.ikesato.bluetooth;
 
-import java.util.Set;
+import java.util.Collection;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 
 public class Bluetooth {
+    public interface OnScanDeviceListener {
+        public void onFoundDevice(BluetoothDevice device);
+        public void onFinishedScan();
+    }
+
     private BluetoothAdapter mBtAdapter;
+    private OnScanDeviceListener mListener = null;
+
+    /**
+     * Constructor
+     */
+    public Bluetooth() {
+        mBtAdapter = BluetoothAdapter.getDefaultAdapter();
+    }
 
     /**
      * List paired devices
      * @return already paired devices in the Phone
      */
-    public Set<BluetoothDevice> listPairedDevices() {
-        mBtAdapter = BluetoothAdapter.getDefaultAdapter();
-        Set<BluetoothDevice> pairedDevices = mBtAdapter.getBondedDevices();
+    public Collection<BluetoothDevice> listPairedDevices() {
+        Collection<BluetoothDevice> pairedDevices = mBtAdapter.getBondedDevices();
         return pairedDevices;
     }
 
-    //public xxx startScanDevices();
+    /**
+     * Scan Bluetooth devices
+     */
+    public void startScan(OnScanDeviceListener listener) {
+        if (listener == null)
+            throw new RuntimeException("you need to specify listener");
+        mListener = listener;
+
+        if (mBtAdapter.isDiscovering())
+            mBtAdapter.cancelDiscovery();
+
+        mBtAdapter.startDiscovery();
+    }
+
+	/**
+	 * Register the receiver
+	 */
+    public void registerReceiver(Context context) {
+        // Register for broadcasts when a device is discovered
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        context.registerReceiver(mReceiver, filter);
+
+        // Register for broadcasts when discovery has finished
+        filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        context.registerReceiver(mReceiver, filter);
+    }
+
+	/**
+	 * Unregister the receiver
+	 */
+    public void unregisterReceiver(Context context) {
+        context.unregisterReceiver(mReceiver);
+    }
+
+    // The BroadcastReceiver that listens for discovered devices and
+    // changes the title when discovery is finished
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // When discovery finds a device
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                mListener.onFoundDevice(device);
+            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                // When discovery is finished
+                mListener.onFinishedScan();
+                mListener = null;
+            }
+        }
+    };
 }
--- Android/src/com/ikesato/bt_remocon/MainActivity.java
+++ Android/src/com/ikesato/bt_remocon/MainActivity.java
@@ -1,33 +1,78 @@
 package com.ikesato.bt_remocon;
 
-import java.util.Set;
+import java.util.Collection;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothDevice;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 import com.ikesato.bluetooth.Bluetooth;
 
 public class MainActivity extends Activity {
-	private static final String TAG=MainActivity.class.getSimpleName();
+    private static final String TAG=MainActivity.class.getSimpleName();
+    private Bluetooth mBluetooth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-		Bluetooth bt = new Bluetooth();
-		Set<BluetoothDevice> devices = bt.listPairedDevices();
-		for (BluetoothDevice device : devices) {
-			String s = String.format("%s %d %s %s",
-									 device.getAddress(),
-									 device.getBondState(),
-									 device.getName(),
-									 device.getBluetoothClass().toString());
-			Log.v(TAG, s);
-		}
+        mBluetooth = new Bluetooth();
+
+        findViewById(R.id.list_devices_button).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v(TAG, "list devices");
+                Log.v(TAG, "============");
+                Collection<BluetoothDevice> devices = mBluetooth.listPairedDevices();
+                for (BluetoothDevice device : devices) {
+                    String s = String.format("device: %s %d %s %s",
+                                             device.getAddress(),
+                                             device.getBondState(),
+                                             device.getName(),
+                                             device.getBluetoothClass().toString());
+                    Log.v(TAG, s);
+                }
+            }
+        });
+
+        findViewById(R.id.scan_devices_button).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v(TAG, "scan devices");
+                Log.v(TAG, "============");
+                mBluetooth.startScan(new Bluetooth.OnScanDeviceListener() {
+                    @Override
+                    public void onFoundDevice(BluetoothDevice device) {
+                        String s = String.format("found: %s %d %s %s",
+                                                 device.getAddress(),
+                                                 device.getBondState(),
+                                                 device.getName(),
+                                                 device.getBluetoothClass().toString());
+                        Log.v(TAG, s);
+                    }
+
+                    @Override
+                    public void onFinishedScan() {
+                        Log.v(TAG, "finished scan devices.");
+                    }
+                });
+            }
+        });
+    }
+
+    public void onResume() {
+        super.onResume();
+        mBluetooth.registerReceiver(this);
+    }
+
+    public void onPause() {
+        super.onPause();
+        mBluetooth.unregisterReceiver(this);
     }
 
     @Override
